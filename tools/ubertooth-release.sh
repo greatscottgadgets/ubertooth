f741717637bb207efdaabab09ff3b49093f6cfef
Dominic Spill <dominicgs@gmail.com>
2012-10-09 01:03:25 +1100

    Release notes and procedure for 2012-10-R1

--- tools/ubertooth-release.sh
+++ tools/ubertooth-release.sh
@@ -19,7 +19,7 @@ top="`pwd`/../release"
 releasename="ubertooth-${branch}"
 targetdir="${top}/${releasename}"
 # Previous release, this is laziness to not have to rebuild the hardware files
-originaldir="${top}/ubertooth-${1}"
+originaldir="${top}/ubertooth-r${1}"
 
 mkdir -p ${targetdir}
 git archive --format=tar  HEAD | (cd ${targetdir} && tar xf -)
@@ -36,60 +36,57 @@ make
 # Software
 ############################
 export BOARD=UBERTOOTH_ONE
-mkdir ${dir}/ubertooth-one-firmware-bin
+mkdir ${targetdir}/ubertooth-one-firmware-bin
 cd ${targetdir}/firmware
 make clean
 make bluetooth_rxtx
-cp bluetooth_rxtx.hex ${targetdir}/ubertooth-one-firmware-bin/
-cp bluetooth_rxtx.bin ${targetdir}/ubertooth-one-firmware-bin/
-cp bluetooth_rxtx.dfu ${targetdir}/ubertooth-one-firmware-bin/
-cd ${targetdir}/firmware/bootloader
+cp bluetooth_rxtx/bluetooth_rxtx.hex ${targetdir}/ubertooth-one-firmware-bin/
+cp bluetooth_rxtx/bluetooth_rxtx.bin ${targetdir}/ubertooth-one-firmware-bin/
+cp bluetooth_rxtx/bluetooth_rxtx.dfu ${targetdir}/ubertooth-one-firmware-bin/
 make bootloader
-cp bootloader.hex ${targetdir}/ubertooth-one-firmware-bin/
-cp bootloader.bin ${targetdir}/ubertooth-one-firmware-bin/
-cd ${targetdir}/firmware/assembly_test
+cp bootloader/bootloader.hex ${targetdir}/ubertooth-one-firmware-bin/
+cp bootloader/bootloader.bin ${targetdir}/ubertooth-one-firmware-bin/
 make assembly_test
-cp assembly_test.hex ${targetdir}/ubertooth-one-firmware-bin/
-cp assembly_test.bin ${targetdir}/ubertooth-one-firmware-bin/
+cp assembly_test/assembly_test.hex ${targetdir}/ubertooth-one-firmware-bin/
+cp assembly_test/assembly_test.bin ${targetdir}/ubertooth-one-firmware-bin/
 make clean
 
 export BOARD=UBERTOOTH_ZERO
 mkdir ${targetdir}/ubertooth-zero-firmware-bin
-cd ${targetdir}/firmware/bluetooth_rxtx
+cd ${targetdir}/firmware/
 make bluetooth_rxtx
-cp bluetooth_rxtx.hex ${targetdir}/ubertooth-zero-firmware-bin/
-cp bluetooth_rxtx.bin ${targetdir}/ubertooth-zero-firmware-bin/
-cp bluetooth_rxtx.dfu ${targetdir}/ubertooth-zero-firmware-bin/
-cd ${targetdir}/firmware/bootloader
+cp bluetooth_rxtx/bluetooth_rxtx.hex ${targetdir}/ubertooth-zero-firmware-bin/
+cp bluetooth_rxtx/bluetooth_rxtx.bin ${targetdir}/ubertooth-zero-firmware-bin/
+cp bluetooth_rxtx/bluetooth_rxtx.dfu ${targetdir}/ubertooth-zero-firmware-bin/
 make bootloader
-cp bootloader.hex ${targetdir}/ubertooth-zero-firmware-bin/
-cp bootloader.bin ${targetdir}/ubertooth-zero-firmware-bin/
+cp bootloader/bootloader.hex ${targetdir}/ubertooth-zero-firmware-bin/
+cp bootloader/bootloader.bin ${targetdir}/ubertooth-zero-firmware-bin/
 make clean
 
 ############################
-# HArdware
+# Hardware
 ############################
-mkdir ${targetdir}/pogoprog-hardware
-mkdir ${targetdir}/pogoprog-hardware/gerbers
-cp ${originaldir}/hardware/pogoprog/pogoprog-schematic.pdf ${targetdir}/pogoprog-hardware
-cp ${originaldir}/hardware/pogoprog/pogoprog-assembly.pdf ${targetdir}/pogoprog-hardware
-cp ${originaldir}/hardware/pogoprog/pogoprog-*.g* ${tagetdir}/pogoprog-hardware/gerbers
-cp ${originaldir}/hardware/pogoprog/pogoprog.drl ${targetdir}/pogoprog-hardware/gerbers
-cp ${originaldir}/hardware/pogoprog/pogoprog.csv ${targetdir}/pogoprog-hardware/pogoprog-bom.csv
-mkdir ${targetdir}/ubertooth-one-hardware
-mkdir ${targetdir}/ubertooth-one-hardware/gerbers
-cp ${originaldir}/hardware/ubertooth-one/ubertooth-one-schematic.pdf ${targetdir}/ubertooth-one-hardware
-cp ${originaldir}/hardware/ubertooth-one/ubertooth-one-assembly.pdf ${targetdir}/ubertooth-one-hardware
-cp ${originaldir}/hardware/ubertooth-one/ubertooth-one-*.g* ${targetdir}/ubertooth-one-hardware/gerbers
-cp ${originaldir}/hardware/ubertooth-one/ubertooth-one.drl ${targetdir}/ubertooth-one-hardware/gerbers
-cp ${originaldir}/hardware/ubertooth-one/ubertooth-one.csv ${targetdir}/ubertooth-one-hardware/ubertooth-one-bom.csv
-mkdir ${targetdir}/tc13badge-hardware
-mkdir ${targetdir}/tc13badge-hardware/gerbers
-cp ${originaldir}/hardware/tc13badge/tc13badge-schematic.pdf ${targetdir}/tc13badge-hardware
-cp ${originaldir}/hardware/tc13badge/tc13badge-assembly.pdf ${targetdir}/tc13badge-hardware
-cp ${originaldir}/hardware/tc13badge/tc13badge-*.g* ${targetdir}/tc13badge-hardware/gerbers
-cp ${originaldir}/hardware/tc13badge/tc13badge.drl ${targetdir}/tc13badge-hardware/gerbers
-cp ${originaldir}/hardware/tc13badge/tc13badge.csv ${targetdir}/tc13badge-hardware/tc13badge-bom.csv
+#mkdir ${targetdir}/pogoprog-hardware
+#mkdir ${targetdir}/pogoprog-hardware/gerbers
+cp ${originaldir}/hardware/pogoprog/pogoprog-schematic.pdf ${targetdir}/hardware/pogoprog
+cp ${originaldir}/hardware/pogoprog/pogoprog-assembly.pdf ${targetdir}/hardware/pogoprog
+#cp ${originaldir}/hardware/pogoprog/pogoprog-*.g* ${tagetdir}/hardware/pogoprog/gerbers
+#cp ${originaldir}/hardware/pogoprog/pogoprog.drl ${targetdir}/hardware/pogoprog/gerbers
+cp ${originaldir}/hardware/pogoprog/pogoprog-bom.csv ${targetdir}/hardware/pogoprog/pogoprog-bom.csv
+#mkdir ${targetdir}/ubertooth-one-hardware
+#mkdir ${targetdir}/ubertooth-one-hardware/gerbers
+cp ${originaldir}/hardware/ubertooth-one/ubertooth-one-schematic.pdf ${targetdir}/hardware/ubertooth-one
+cp ${originaldir}/hardware/ubertooth-one/ubertooth-one-assembly.pdf ${targetdir}/hardware/ubertooth-one
+#cp ${originaldir}/hardware/ubertooth-one/ubertooth-one-*.g* ${targetdir}/hardware/ubertooth-one/gerbers
+#cp ${originaldir}/hardware/ubertooth-one/ubertooth-one.drl ${targetdir}/hardware/ubertooth-one/gerbers
+cp ${originaldir}/hardware/ubertooth-one/ubertooth-one-bom.csv ${targetdir}/hardware/ubertooth-one/ubertooth-one-bom.csv
+#mkdir ${targetdir}/tc13badge-hardware
+#mkdir ${targetdir}/tc13badge-hardware/gerbers
+cp ${originaldir}/hardware/tc13badge/tc13badge-schematic.pdf ${targetdir}/hardware/tc13badge/
+cp ${originaldir}/hardware/tc13badge/tc13badge-assembly.pdf ${targetdir}/hardware/tc13badge/
+#cp ${originaldir}/hardware/tc13badge/tc13badge-*.g* ${targetdir}/hardware/tc13badge/gerbers
+#cp ${originaldir}/hardware/tc13badge/tc13badge.drl ${targetdir}/hardware/tc13badge/gerbers
+cp ${originaldir}/hardware/tc13badge/tc13badge-bom.csv ${targetdir}/hardware/tc13badge/tc13badge-bom.csv
 
 ############################
 # Clean up
@@ -102,4 +99,4 @@ rm ${targetdir}/.gitignore
 # Archive
 ############################
 cd ${top}
-tar -zcf ${releasename}.tgz ${releasename}/
+tar -zcf ${releasename}.tar.gz ${releasename}/
