df31b20e3af689203743e6c0c76bad3c58451766
Dominic Spill <dominicgs@gmail.com>
2013-02-16 18:34:04 +1100

    Stop segfaults when ubertooth-rx exits

--- host/bluetooth_rxtx/ubertooth-rx.c
+++ host/bluetooth_rxtx/ubertooth-rx.c
@@ -129,7 +129,7 @@ int main(int argc, char *argv[])
 		rx_live(devh, pn);
 
 		// Print AFH map from piconet
-		btbb_print_afh_map(pn);
+		//btbb_print_afh_map(pn);
 
 		ubertooth_stop(devh);
 	} else {
