9ee38b4e8d20b558b10a37132030250a15c09a96
Dominic Spill <dominicgs@gmail.com>
2014-02-26 21:49:46 +0000

    Update release in homebrew formula and ass MacPorts portfile.

--- host/packaging/ubertooth.rb
+++ host/packaging/ubertooth.rb
@@ -2,15 +2,15 @@ require "formula"
 
 class Ubertooth < Formula
   homepage "https://github.com/greatscottgadgets/ubertooth"
-  url "https://github.com/greatscottgadgets/ubertooth/archive/2014-02-R1.tar.gz"
-  sha1 "1ab0d89a41f5443f3d65ebb9e05c3e5338de2ba4"
-  version "2014-02-R1"
+  url "https://github.com/greatscottgadgets/ubertooth/archive/2014-02-R2.tar.gz"
+  sha1 "773eab8b6440dcf7fd6bcc83d9e365c3239dd5f3"
+  version "2014-02-R2"
 
   head "https://github.com/greatscottgadgets/ubertooth.git"
 
   resource "pyusb" do
     url "https://pypi.python.org/packages/source/p/pyusb/pyusb-1.0.0b1.tar.gz"
-    sha256 "6fa787840baa8c6a041e370bf381127aae5fb44c820ba655f966b7da4de6279f"
+    sha1 "f0ca8bdfbb59645ea73976f9a3cb2b0701667148"
   end
 
   depends_on "cmake" => :build
