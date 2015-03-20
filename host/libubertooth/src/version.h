3bba8056e1fb11440a699bb187452c8a7337c9b6
Dominic Spill <dominicgs@gmail.com>
2015-03-20 16:33:00 +0000

    Fix "this file is part of <project>" error

--- host/libubertooth/src/version.h
+++ host/libubertooth/src/version.h
@@ -1,7 +1,7 @@
 /*
  * Copyright 2014 Dominic Spill
  *
- * This file is part of libbtbb.
+ * This file is part of Ubertooth.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
