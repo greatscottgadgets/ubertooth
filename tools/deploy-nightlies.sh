1438b40bad21322d6ab3f56aa9d7b7f73c89aaae
Dominic Spill <dominicgs@gmail.com>
2018-12-19 10:11:25 -0700

    Fix path to git file to add

--- tools/deploy-nightlies.sh
+++ tools/deploy-nightlies.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 REPO=greatscottgadgets/ubertooth-nightlies
 PUBLICATION_BRANCH=gh-pages
-FILE_TO_DEPLOY=$ARTEFACT_BASE/$BUILD_NAME.tar.xz
+FILE_TO_DEPLOY=
 set -x
-cd $ARTEFACT_BASE
 # Checkout the branch
 git clone --branch=$PUBLICATION_BRANCH https://${GITHUB_TOKEN}@github.com/$REPO.git publish
 cd publish
 # Update pages
-cp $FILE_TO_DEPLOY .
+cp $ARTEFACT_BASE/$BUILD_NAME.tar.xz .
 # Commit and push latest version
-git add $FILE_TO_DEPLOY
+git add $BUILD_NAME.tar.xz
 git config user.name  "Travis"
 git config user.email "travis@travis-ci.org"
 git commit -m "FIXME: useful information here"
