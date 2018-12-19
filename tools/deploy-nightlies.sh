d91027393bdf4059582fa08cd6dfdd40609bf983
Dominic Spill <dominicgs@gmail.com>
2018-12-19 09:21:26 -0700

    Try using a script to deploy

---
+++ tools/deploy-nightlies.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+REPO=greatscottgadgets/ubertooth-nightlies
+PUBLICATION_BRANCH=gh-pages
+FILE_TO_DEPLOY=$ARTEFACT_BASE/$BUILD_NAME.tar.xz
+set -x
+# Checkout the branch
+git clone --branch=$PUBLICATION_BRANCH https://${GITHUB_TOKEN}@github.com/$REPO publish 2>&1 > /dev/null
+cd publish
+# Update pages
+cp $FILE_TO_DEPLOY .
+# Commit and push latest version
+git add $FILE_TO_DEPLOY
+git config user.name  "Travis"
+git config user.email "travis@travis-ci.org"
+git commit -m "FIXME: useful information here"
+if [ "$?" != "0" ]; then
+    echo "Looks like the commit failed"
+fi
+git push -fq origin $PUBLICATION_BRANCH 2>&1 > /dev/null
