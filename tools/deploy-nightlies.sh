8c46b51d3f4c7945f980c3c68af2f4b28297b3d5
Dominic Spill <dominicgs@gmail.com>
2018-12-19 11:37:10 -0700

    trying to write an index

--- tools/deploy-nightlies.sh
+++ tools/deploy-nightlies.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
 REPO=greatscottgadgets/ubertooth-nightlies
 PUBLICATION_BRANCH=gh-pages
-FILE_TO_DEPLOY=
 set -x
 # Checkout the branch
 git clone --branch=$PUBLICATION_BRANCH https://${GITHUB_TOKEN}@github.com/$REPO.git publish
 cd publish
 # Update pages
 cp $ARTEFACT_BASE/$BUILD_NAME.tar.xz .
+FILES=`ls -ltr | grep -v index.html | xargs`
+INDEX=`cat <<EOF
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html><head>
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+	<title>Ubertooth Nightly Builds</title>
+</head>
+<body>
+$FILES
+</body></html>
+EOF
+`
+echo $INDEX > index.html
 # Commit and push latest version
-git add $BUILD_NAME.tar.xz
+git add $BUILD_NAME.tar.xz index.html
 git config user.name  "Travis"
 git config user.email "travis@travis-ci.org"
-git commit -m "FIXME: useful information here"
+git commit -m "Build products for $SHORT_COMMIT_HASH, built on $TRAVIS_OS_NAME, log: $TRAVIS_BUILD_WEB_URL"
 if [ "$?" != "0" ]; then
     echo "Looks like the commit failed"
 fi
-git push -fq origin $PUBLICATION_BRANCH 2>&1 > /dev/null
+git push -fq origin $PUBLICATION_BRANCH
