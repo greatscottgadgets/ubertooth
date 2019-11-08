53f44b58c0b71ef204918ab9ccd969c8649429ae
Mike Ryan <mikeryan@lacklustre.net>
2019-11-07 21:15:31 -0800

    le: handle NULL rxbuf under heavy load
    Under heavy load and when processing long packets, all rxbufs can become
    used. In this situation, buffer_get() will return NULL. The existing
    code would dereference the NULL, freezing capture.
    
    This fix checks for NULL and uses the timer interrupt to periodically
    poll for available rxbufs. Once an rxbuf becomes available, capture is
    resumed.
    
    Note that the existing code includes a comment: TODO handle error
    condition. Had that TODO been heeded, untold hours of debugging,
    patches, and lost productivity could have been saved. All for want of an
    error handler.
    
    See #326

--- firmware/bluetooth_rxtx/le_phy.c
+++ firmware/bluetooth_rxtx/le_phy.c
@@ -152,6 +152,8 @@ static void timer1_set_match(uint32_t match);
 static void timer1_clear_match(void);
 static void timer1_wait_fs_lock(void);
 static void timer1_cancel_fs_lock(void);
+static void timer1_wait_buffer(void);
+static void timer1_cancel_buffer(void);
 static void blink(int tx, int rx, int usr);
 static void le_dma_init(void);
 static void le_cc2400_strobe_rx(void);
@@ -367,6 +369,7 @@ void le_DMA_IRQHandler(void) {
 					else if (conn_event.num_packets == 2) {
 						cc2400_strobe(SRFOFF);
 						current_rxbuf = buffer_get();
+						// TODO handle NULL
 						finish_conn_event();
 						return;
 					}
@@ -375,13 +378,13 @@ void le_DMA_IRQHandler(void) {
 				// get a new packet
 				// TODO handle error transition
 				current_rxbuf = buffer_get();
-
-				// restart DMA and SSP
-				le_dma_init();
-				dio_ssp_start();
-
-				// wait for FS_LOCK in background
-				timer1_wait_fs_lock();
+				if (current_rxbuf == NULL) {
+					// if all buffers are in use, wait for one to free up in background
+					timer1_wait_buffer();
+				} else {
+					// wait for FS_LOCK in background
+					timer1_wait_fs_lock();
+				}
 			}
 		}
 
@@ -563,6 +566,15 @@ static void timer1_cancel_fs_lock(void) {
 	T1MCR &= ~TMCR_MR2I;
 }
 
+static void timer1_wait_buffer(void) {
+	T1MR3 = NOW + USEC(100);
+	T1MCR |= TMCR_MR3I;
+}
+
+static void timer1_cancel_wait_buffer(void) {
+	T1MCR &= ~TMCR_MR3I;
+}
+
 void TIMER1_IRQHandler(void) {
 	// MR0: connection events
 	if (T1IR & TIR_MR0_Interrupt) {
@@ -655,6 +667,10 @@ void TIMER1_IRQHandler(void) {
 
 		// if FS is locked, strobe RX and clear interrupt
 		if (cc2400_status() & FS_LOCK) {
+			// restart DMA and SSP
+			le_dma_init();
+			dio_ssp_start();
+
 			le_cc2400_strobe_rx();
 			T1MCR &= ~TMCR_MR2I;
 		}
@@ -662,6 +678,26 @@ void TIMER1_IRQHandler(void) {
 		// if FS is not locked, check again in 3 us
 		else {
 			timer1_wait_fs_lock();
+		}
+	}
+
+	// check if an rxbuf is available and transition to FS_LOCK/RX
+	if (T1IR & TIR_MR3_Interrupt) {
+		T1IR = TIR_MR3_Interrupt;
+
+		current_rxbuf = buffer_get();
+		if (current_rxbuf == NULL) {
+			timer1_wait_buffer();
+		} else {
+			if (cc2400_status() & FS_LOCK) {
+				// restart DMA and SSP
+				le_dma_init();
+				dio_ssp_start();
+
+				le_cc2400_strobe_rx();
+			} else {
+				timer1_wait_fs_lock();
+			}
 		}
 	}
 }
