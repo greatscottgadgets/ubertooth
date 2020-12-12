ae2ff41ce03c35353e329790efce343cb6263fbd
Mike Ryan <mikeryan@lacklustre.net>
2020-12-12 14:31:40 -0800

    firmware: bump API to match userspace changes

--- firmware/bluetooth_rxtx/ubertooth_usb.c
+++ firmware/bluetooth_rxtx/ubertooth_usb.c
@@ -91,7 +91,7 @@ static u8 abDescriptors[] = {
 	MAX_PACKET_SIZE0,  		// bMaxPacketSize
 	LE_WORD(ID_VENDOR),		// idVendor
 	LE_WORD(ID_PRODUCT),		// idProduct
-	LE_WORD(0x0106),		// bcdDevice
+	LE_WORD(0x0107),		// bcdDevice
 	0x01,              		// iManufacturer
 	0x02,              		// iProduct
 	0x03,              		// iSerialNumber
